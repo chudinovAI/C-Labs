90e3889ca7a6ec2673e0a9a4d42e556a49be908a
propheT <122778331+chudinovAI@users.noreply.github.com>
2025-04-15 18:58:29 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/C_Labs.dir/main.cpp.o.ddi.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/C_Labs.dir/main.cpp.o.ddi: \
- /home/proph37/CLionProjects/C-Labs/main.cpp /usr/include/stdc-predef.h \
- /home/proph37/CLionProjects/C-Labs/Lab-7/Number\ 3/number_3.h \
- /home/proph37/CLionProjects/C-Labs/Lab-7/Number\ 4/number_4.h
